7a48441bb03300962f7a1f48f5e02d15ff360cb3
jocelyndrean <jocelyn.drean@gmail.com>
2014-04-25 18:20:24 -0400

    Supression des concerts sans musiques

--- app/controllers/concerts_controller.rb
+++ app/controllers/concerts_controller.rb
@@ -6,12 +6,12 @@ class ConcertsController < ApplicationController
 	    if params[:query]
 	    	@songs = @client.search_songs(params[:query])
 	    else
-	    	metro_area = remote.location_search_geo(49.1811,-0.3712, :per_page => 1).results
+	    	metro_area = remote.location_search_geo(49.158611,-0.390833, :per_page => 1).results
 	    	metro_area_id = 'sk:' + metro_area.shift.metro_area.id.to_s
 	    	today = Time.new.strftime("%Y-%m-%d")
 	    	today_next_year = (Time.new + 1.years).strftime("%Y-%m-%d")
 	    	@concerts = remote.events(:location => metro_area_id, :per_page => '50', :min_date => today, :max_date => today_next_year, :page => 1).results
-	    	@concerts.delete_if{|c| c.popularity < 0.001}
+	    	@concerts.delete_if{|c| c.popularity < 0.0008}
 	    	@hash_widget = Hash.new
 	    	@concerts.each do |c|
 	    		artist_name = c.performances.shift.display_name
@@ -20,7 +20,7 @@ class ConcertsController < ApplicationController
 	    		songs = songs.first 5
 	    		@hash_widget[c.id] = songs
 	    	end
-
+	    	@concerts.delete_if{|c| @hash_widget[c.id].empty? }
 	    end
   	end
 end
